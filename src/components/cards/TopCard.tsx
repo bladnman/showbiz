f0ab5dcaa15972610c1fbcadf9fb2eb4b8d25298
Matt Maher <matt.maher@sony.com>
2022-11-10 05:31:46 -0800

    search cards

--- src/components/cards/TopCard.tsx
+++ src/components/cards/TopCard.tsx
@@ -1,12 +1,11 @@
-import { Box, Rating, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { useCallback } from "react";
-import { Movie, Person, Season, TvShow } from "../../TMDB/types";
+import { Movie, Person, TvShow } from "../../TMDB/types";
 import {
   isMovieType,
   isPersonType,
   isTvShowType,
 } from "../../TMDB/utils/duckTyping";
-import { fLeft } from "../../utils/MU";
 import MovieCard from "./MovieCard";
 import PersonCard from "./PersonCard";
 import TvShowCard from "./TvShowCard";
@@ -22,68 +21,3 @@ export default function TopCard({ item }: { item: Movie | TvShow | Person }) {
   if (!item) return null;
   return <Box onClick={() => console.log(`🐽 Item`, item)}>{renderCard()}</Box>;
 }
-const TvMeta = ({ show }: { show: TvShow }) => {
-  return (
-    <Stack
-      direction={"row"}
-      gap={2}
-      justifyContent="space-between"
-      alignItems={"center"}
-    >
-      <ShowRating show={show} />
-      <TvMetaSeasonEpisodes show={show} />
-    </Stack>
-  );
-};
-const TvMetaSeasonEpisodes = ({ show }: { show: TvShow }) => {
-  const countEpisodes = (seasons?: Season[]) => {
-    let total = 0;
-    seasons &&
-      seasons.forEach((season) => {
-        total += season?.episodeCount ?? 0;
-      });
-    return total;
-  };
-  const episodeCount = countEpisodes(show?.seasons);
-  if (episodeCount < 1) return null;
-  return (
-    <Stack direction={"row"} gap={2}>
-      <Typography variant="caption" color="text.secondary">
-        Seasons: {show.seasons?.length}
-      </Typography>
-
-      <Typography variant="caption" color="text.secondary">
-        Episodes: {episodeCount}
-      </Typography>
-    </Stack>
-  );
-};
-const MovieMeta = ({ show }: { show: Movie }) => {
-  const year = fLeft(show.releaseDate, "-");
-  return (
-    <Stack
-      direction={"row"}
-      gap={2}
-      justifyContent="space-between"
-      alignItems={"center"}
-    >
-      <ShowRating show={show} />
-
-      <Typography variant="caption" color="text.secondary">
-        Released: {year}
-      </Typography>
-    </Stack>
-  );
-};
-const ShowRating = ({ show }: { show: Movie | TvShow }) => {
-  const rating = (show.voteAverage ?? 0) / 2;
-  return (
-    <Rating
-      name="read-only"
-      value={rating}
-      readOnly
-      precision={0.25}
-      size="small"
-    />
-  );
-};
