a142f4c5b3a9a1f9477af74a9ce45bad85114073
Matt Maher <matt.maher@sony.com>
2022-12-10 18:28:45 -0800

    vite config

--- src/services/firestore/fire_const.ts
+++ src/services/firestore/fire_const.ts
@@ -1,4 +1,4 @@
-const level_prefix = "t_";
+const level_prefix = "matt_";
 
 export const FIRE_SHOWS_COLLECTION_NAME = `${level_prefix}shows`; // matt_shows, shows
 export const FIRE_CUSTOM_DATA_COLLECTION_NAME = `${level_prefix}custom_data`;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
+  // build: {
+  //   minify: false,
+  // },
+  // esbuild: {
+  //   minifySyntax: false,
+  //   minifyWhitespace: false,
+  //   minifyIdentifiers: false,
+  // },
 });
